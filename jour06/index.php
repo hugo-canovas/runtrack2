ee790cda10279d06f32d003a8b5c5c206028460b
hugo-canovas <canovashugo83@gmail.com>
2023-01-09 10:37:33 +0100

    Rendu jour 6

---
+++ jour06/index.php
@@ -0,0 +1,26 @@
+<form action="index.php" method="get">
+    <select name="style" id="style">
+        <option value="">Choisissez un style CSS</option>
+        <option value="style1">1</option>
+        <option value="style2">2</option>
+        <option value="style3">3</option>
+    </select>
+    <button type="submit">Valider</button>
+</form>
+
+<?php
+
+    $myStyle = $_GET['style'];
+
+    if($myStyle == "style1"){?>
+        <link rel="stylesheet" href="./style1.css"> 
+<?php        
+    }else if($myStyle == "style2"){?>
+        <link rel="stylesheet" href="./style2.css">
+<?php
+    }else if($myStyle == "style3"){?>
+        <link rel="stylesheet" href="./style3.css">
+<?php
+    }
+
+?>
