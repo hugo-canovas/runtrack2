d9d267ae6d999df8fd8f105f756028e2f30c8750
hugo-canovas <canovashugo83@gmail.com>
2023-01-13 15:50:22 +0100

    Rendu jour 6 avec changement mineur

--- jour06/index.php
+++ jour06/index.php
@@ -5,11 +5,11 @@
         <option value="style2">2</option>
         <option value="style3">3</option>
     </select>
-    <button type="submit">Valider</button>
+    <button type="submit" name='valider'>Valider</button>
 </form>
 
 <?php
-
+if(isset($_GET['valider'])){
     $myStyle = $_GET['style'];
 
     if($myStyle == "style1"){?>
@@ -22,5 +22,6 @@
         <link rel="stylesheet" href="./style3.css">
 <?php
     }
+}
 
 ?>
