eed04ad2c55ae768b7057e0b96b22c8e32ba5c2c
hugocanovas <canovashugo83@gmail.com>
2023-01-14 19:47:35 +0100

    Ajout des cookies X et O

--- jour08/job05/index.php
+++ jour08/job05/index.php
@@ -1,4 +1,9 @@
 <?php
+    $i = 1;
+    $round = 1;
+    $j1 = "X";
+    $j2 = "O";
+// CONNEXION ET DECONNEXION DES JOUEURS
 
     if(isset($_POST['login'])){
         setcookie('joueur_1', $_POST['joueur_1'], time()+3600);
@@ -18,173 +23,53 @@
             </form>
 <?php 
     }
-    
+?>
+
+<?php
+// AFFICHAGE DU TABLEAU SI JOUEUR CONNECTE SINON FORMULAIRE DE CONNEXION
+
     if(isset($_COOKIE['joueur_1']) && isset($_COOKIE['joueur_2'])){ 
 ?>
         <table style='width: 500px; height: 500px;'>
-            <tr>
                 <form method='post' action='index.php'>
-<?php
-                    if(isset($_POST['1'])){
-                        setcookie('hg', "X", time()+3600);
-                        header("Refresh:0");
-                    }
+<?php               
+                    for($ligne = 0; $ligne < 3; $ligne++){
+                        echo'<tr>';
+                        for($colone = 0; $colone < 3; $colone++){   
+                            if(isset($_POST[$i])){
+                                setcookie($i, $i, time()+3600);
+                                header('Refresh:0');
+                            }else if(isset($_POST['restart'])){
+                                setcookie($i, '', time()-3600);
+                                unset($_COOKIE[$i]);
+                                header('Refresh:0');
+                            }
 
-                    if(isset($_COOKIE['hg'])){
-?>
-                        <td><?php echo $_COOKIE['hg'] ?></td>
-<?php
-                    }else{
-?>
-                        <td><button type='submit' name='1' value='-' style='width: 75px;height: 75px;'></button></td>
-<?php
+                            if(isset($_COOKIE[$i])){
+                                if($round == 1){
+                                    // setcookie($i, $j1, time()+3600);                     
+                                    echo '<td>X</td>';         
+                                    $round++;
+                                }else if($round == 2){
+                                    // setcookie($i, $j2, time()+3600);
+                                    echo '<td>O</td>';
+                                    $round--;
+                                }
+                            }else{
+                                 echo '<td><button type="submit" name="'.$i.'" style="width: 50px; height: 50px;"></button></td>';
+                            }
+                           $i++;                 
+                        }
+                        echo "<br>";
+                        echo '</tr>';
                     }
-
-                    if(isset($_POST['2'])){
-                        setcookie('hm', "X", time()+3600);
-                        header("Refresh:0");
-                    }
-
-                    if(isset($_COOKIE['hm'])){
-?>
-                        <td><?php echo $_COOKIE['hm'] ?></td>
-<?php
-                    }else{
-?>
-                        <td><button type='submit' name='2' value='-' style='width: 75px;height: 75px;'></button></td>
-<?php
-                    }
-
-                    if(isset($_POST['3'])){
-                        setcookie('hd', "X", time()+3600);
-                        header("Refresh:0");
-                    }
-
-                    if(isset($_COOKIE['hd'])){
-?>
-                        <td><?php echo $_COOKIE['hd'] ?></td>
-<?php
-                    }else{
-?>
-                        <td><button type='submit' name='3' value='-' style='width: 75px;height: 75px;'></button></td>
-<?php
-                    }
-?>
-            </tr>
-            <tr>
-<?php
-
-                    if(isset($_POST['4'])){
-                        setcookie('mg', "X", time()+3600);
-                        header("Refresh:0");
-                    }
-
-                    if(isset($_COOKIE['mg'])){
-?>
-                        <td><?php echo $_COOKIE['mg'] ?></td>
-<?php
-                    }else{
-?>
-                        <td><button type='submit' name='4' value='-' style='width: 75px;height: 75px;'></button></td>
-<?php
-                    }
-
-                    if(isset($_POST['5'])){
-                        setcookie('mm', "X", time()+3600);
-                        header("Refresh:0");
-                    }
-
-                    if(isset($_COOKIE['mm'])){
-?>
-                        <td><?php echo $_COOKIE['mm'] ?></td>
-<?php
-                    }else{
-?>
-                        <td><button type='submit' name='5' value='-' style='width: 75px;height: 75px;'></button></td>
-<?php
-                    }
-
-                    if(isset($_POST['6'])){
-                        setcookie('md', "X", time()+3600);
-                        header("Refresh:0");
-                    }
-
-                    if(isset($_COOKIE['md'])){
-?>
-                        <td><?php echo $_COOKIE['md'] ?></td>
-<?php
-                    }else{
-?>
-                        <td><button type='submit' name='6' value='-' style='width: 75px;height: 75px;'></button></td>
-<?php
-                    }
-?>
-            </tr>
-            <tr>
-<?php
                    
-                    if(isset($_POST['7'])){
-                        setcookie('bg', "X", time()+3600);
-                        header("Refresh:0");
-                    }
-
-                    if(isset($_COOKIE['bg'])){
-?>
-                        <td><?php echo $_COOKIE['bg'] ?></td>
-<?php
-                    }else{
-?>
-                        <td><button type='submit' name='7' value='-' style='width: 75px;height: 75px;'></button></td>
-<?php
-                    }
-              
-                    if(isset($_POST['8'])){
-                        setcookie('bm', "X", time()+3600);
-                        header("Refresh:0");
-                    }
-
-                    if(isset($_COOKIE['bm'])){
-?>
-                        <td><?php echo $_COOKIE['bm'] ?></td>
-<?php
-                    }else{
-?>
-                        <td><button type='submit' name='8' value='-' style='width: 75px;height: 75px;'></button></td>
-<?php
-                    }
-  
-                    if(isset($_POST['9'])){
-                        setcookie('bd', "X", time()+3600);
-                        header("Refresh:0");
-                    }
-
-                    if(isset($_COOKIE['bd'])){
-?>
-                        <td><?php echo $_COOKIE['bd'] ?></td>
-<?php
-                    }else{
-?>
-                        <td><button type='submit' name='9' value='-' style='width: 75px;height: 75px;'></button></td>
-<?php
-                    }
-?>
-            </tr>
+                    
+?>    
         </table>
         <button type='submit' name='restart'>Rejouer</button>
     </form>
 <?php
-                    if(isset($_POST['restart'])){
-                        setcookie('hg', '', time()+3600);
-                        setcookie('hm', '', time()+3600);
-                        setcookie('hd', '', time()+3600);
-                        setcookie('mg', '', time()+3600);
-                        setcookie('mm', '', time()+3600);
-                        setcookie('md', '', time()+3600);
-                        setcookie('bg', '', time()+3600);
-                        setcookie('bm', '', time()+3600);
-                        setcookie('bd', '', time()+3600);
-                        header('Refresh:0');
-                    }
     }else{ ?>
         <form action='index.php' method='post'>
                 <input type='text' name='joueur_1'>
@@ -193,6 +78,11 @@
             </form>
 <?php   
     }
+?>
+
+<?php 
 
     
 ?>
+
+
