677a09ad0ea484c8c1a5037cc2e7ce43e139b1e7
hugocanovas <canovashugo83@gmail.com>
2023-01-17 02:22:54 +0100

    ajout des functions

--- jour08/job05/index.php
+++ jour08/job05/index.php
@@ -1,77 +1,65 @@
 <?php
-    $i = 1;
-    $round = 1;
-    $j1 = "X";
-    $j2 = "O";
+
+    session_start();
+    
+    if(!isset($_SESSION['symbol'])){
+        $_SESSION['symbol'] = "X";
+    }
+    
 // CONNEXION ET DECONNEXION DES JOUEURS
 
     if(isset($_POST['login'])){
-        setcookie('joueur_1', $_POST['joueur_1'], time()+3600);
-        setcookie('joueur_2', $_POST['joueur_2'], time()+3600);
+        $_SESSION['joueur_1'] = $_POST['joueur_1'];
+        $_SESSION['joueur_2'] = $_POST['joueur_2'];
         header("Refresh:0");
     }else if(isset($_POST['logout'])){
-        setcookie('joueur_1', '', time()-3600);
-        setcookie('joueur_2', '', time()-3600);
+        unset($_SESSION['joueur_1']);
+        unset($_SESSION['joueur_2']);
+        
         header("Refresh:0");
     }
 
-    if(isset($_COOKIE['joueur_1']) && isset($_COOKIE['joueur_2'])){
-        echo "le joueur ".$_COOKIE['joueur_1']." commence la partie !";
-?>
-            <form action='index.php' method='post'>
-                <button type='submit' name='logout'>Déconnexion</button>
-            </form>
-<?php 
-    }
+    
 ?>
 
 <?php
 // AFFICHAGE DU TABLEAU SI JOUEUR CONNECTE SINON FORMULAIRE DE CONNEXION
 
-    if(isset($_COOKIE['joueur_1']) && isset($_COOKIE['joueur_2'])){ 
+    if(isset($_SESSION['joueur_1']) && isset($_SESSION['joueur_2'])){ 
+        echo "le joueur ".$_SESSION['joueur_1']." commence la partie !";
 ?>
-        <table style='width: 500px; height: 500px;'>
-                <form method='post' action='index.php'>
+        <form action='index.php' method='post'>
+           <button type='submit' name='logout'>Déconnexion</button>
+        </form>
+
+        <form method='post' action='index.php'>
+            <table style='width: 500px; height: 500px;'>
 <?php               
+                    $case = 1;
                     for($ligne = 0; $ligne < 3; $ligne++){
                         echo'<tr style="width: 300px; height: 100px;">';
                         for($colone = 0; $colone < 3; $colone++){  
-                            if(isset($_POST[$i])){ 
-                                setcookie($i, $i, time()+3600);
-                                header('Refresh:0');
-                            }else if(isset($_POST['restart'])){
-                                setcookie($i, '', time()-3600);
-                                unset($_COOKIE[$i]);
+?>
+                            <td style="height: 100px; width: 100px;">
+                                <button style ="height: 100%; width: 100%;" <?php disabled($case) ?> type="submit" name="<?php echo $case ?>" value=""  style="width: 50px; height: 50px;"><?php affichage($case) ?></button>
+                            </td>
+<?php
+                            $case++;
+                            
+                            if(isset($_POST['restart'])){
+                                setcookie(1, '', time()-3600);
+                                setcookie($case, '', time()-3600);
+                                unset($_COOKIE[$case]);
                                 header('Refresh:0');
                             }
-
-                            if(isset($_COOKIE[$i])){
-                                $case = $i;
-                                if($round == 1){
-                                    echo $case;
-                                    setcookie($case, $j1, time()+3600);                     
-                                    echo '<td>'.$j1.'</td>';         
-                                    $round++;
-                                }else if($round == 2){
-                                    setcookie($case, $j2, time()+3600);
-                                    echo $case;
-                                    echo '<td>'.$j2.'</td>';
-                                    $round--;
-                                }
-                            }else{
-                                echo '<td style="height: 100px; width: 100px;"><button style ="height: 100%; width: 100%;" type="submit" name="'.$i.'" style="width: 50px; height: 50px;"></button></td>';
-                            }
-                           $i++;                 
                         }
-                        echo "<br>";
                         echo '</tr>';
                     }
-                   
-                    
+                    echo "<br>";            
 ?>    
-        </table>
-        <button type='submit' name='restart'>Rejouer</button>
-    </form>
+            </table>
+            <button type='submit' name='restart'>Rejouer</button>
+        </form>
 <?php
     }else{ ?>
         <form action='index.php' method='post'>
@@ -85,6 +73,48 @@
 
 <?php 
 
+    function attributionSymbol($case){
+        if($_SESSION['symbol'] == "X"){
+            setcookie($case, $_SESSION['symbol'], time()+3600);
+            $_SESSION['symbol'] = "O";
+            header('Refresh:0');
+        }else if($_SESSION['symbol'] == "O"){
+            setcookie($case, $_SESSION['symbol'], time()+3600);
+            $_SESSION['symbol'] = "X";
+            header('Refresh:0');
+        }
+    }
+
+    // echo $_SESSION['symbol'];
+
+    function affichage($case){
+        if(isset($_COOKIE[$case])){
+            echo $_COOKIE[$case];
+        }
+    }
+
+    function disabled($case){
+        if(isset($_COOKIE[$case])){
+            echo 'disabled';
+        }
+    }
+
+    function playerRound($case){
+        if(isset($_POST[$case])){
+            attributionSymbol($case);
+            
+        }
+    }
+
+    playerRound(1);
+    playerRound(2);
+    playerRound(3);
+    playerRound(4);
+    playerRound(5);
+    playerRound(6);
+    playerRound(7);
+    playerRound(8);
+    playerRound(9);
     
 ?>
 
